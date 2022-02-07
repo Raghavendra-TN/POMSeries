6327dc609604c1aa97fb9a98ea788889de3c8ebd
Raghavendra TN <raghavendra.tn@hp.com>
2022-02-07 16:01:21 +0530

    updated order page as per given review comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,8 +1,11 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 
     String order;
+    private By check = By.id("check");
 
     public void order(){
         System.out.println("order id 1234");
