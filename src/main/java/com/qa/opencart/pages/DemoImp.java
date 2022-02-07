3b364c29301429eb0e3605b119f5d55af1d2d15b
Raghavendra-TN <tnraghavendra.raghu@gmail.com>
2022-02-07 19:56:53 +0530

    Update DemoImp.java

--- src/main/java/com/qa/opencart/pages/DemoImp.java
+++ src/main/java/com/qa/opencart/pages/DemoImp.java
@@ -3,6 +3,7 @@ package com.qa.opencart.pages;
 public class DemoImp {
 
     public void order(){
-        String name = "naveen";
+        String name = "Prashant";
+        int total = 100;
     }
 }
